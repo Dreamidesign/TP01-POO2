6ebd799e64004ee7f9111eeeb5e2f328c242ef1e
apokkalyps <32598875+apokkalyps@users.noreply.github.com>
2018-12-22 20:10:46 +0100

    Update Tests.cpp

--- Tests.cpp
+++ Tests.cpp
@@ -10,17 +10,26 @@ using namespace std;
 void MenuTests ()
 {
 	Trajet * ts = new TrajetSimple ("A", "B", "Velo");
-	cout << ts->toString() << endl; // Doit afficher S:A;B;Velo
+	char * text = ts->toString();
+	cout << text << endl; // Doit afficher S:A;B;Velo
+	delete [] text;
 	
 	Trajet * a = new TrajetSimple ("a", "b", "c");
 	Trajet * b = new TrajetSimple ("b", "d", "c");
 	Trajet * c = new TrajetSimple ("d", "e", "c");
 	
-	TabTrajet tt;
-	tt.AjouterTrajet(a);
-	tt.AjouterTrajet(b);
-	tt.AjouterTrajet(c);
-	Trajet * tc = new TrajetCompose(&tt);
-	cout << tc->toString() << endl;
+	TabTrajet * tt = new TabTrajet;
+	tt->AjouterTrajet(a);
+	tt->AjouterTrajet(b);
+	tt->AjouterTrajet(c);
+	Trajet * tc = new TrajetCompose(tt);
+	text = tc->toString();
+	cout << text << endl;
+	delete[] text;
 	// Doit afficher C(S:a;b;c,S:b;d;c,S:d;e;c)
+	delete ts;/*
+	delete a;
+	delete b;
+	delete c;*/
+	delete tc;
 }
